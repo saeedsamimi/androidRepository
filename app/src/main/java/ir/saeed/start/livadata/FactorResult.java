36e5e118ad0cad7101760362e0c748d419f674dc
saeed samimi <doob.fooy@gmail.com>
2021-10-14 12:32:19 +0330

    ok

--- app/src/main/java/ir/saeed/start/livadata/FactorResult.java
+++ app/src/main/java/ir/saeed/start/livadata/FactorResult.java
@@ -1,14 +1,68 @@
 package ir.saeed.start.livadata;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class FactorResult extends AppCompatActivity {
-
+    TableLayout TL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_factor_result);
+        TL = findViewById(R.id.table_layout);
+        ItemsDB db = new ItemsDB(this);
+        ArrayList<CellItem> items = db.getItemsList();
+        long t = 0;
+        long c = 0;
+        for (int i = 0; i < items.size(); i++) {
+            CellItem item = items.get(i);
+            t += item.getCount()*item.getUnitPrice();
+            c += item.getCount();
+            new ViewHolder(TL, item.setID(i + 1));
+        }
+        new ViewHolder(TL,"Total: ",t,items.size());
+    }
+
+    public class ViewHolder{
+        private LayoutInflater inflater;
+        private TableRow TR;
+        public ViewHolder(TableLayout parent,CellItem item) {
+            inflater = getLayoutInflater();
+            TR = (TableRow) inflater.inflate(R.layout.table_row,parent,false);
+            FindAndTextView(R.id.item_name_view,item.getName());
+            FindAndTextView(R.id.number_view, String.valueOf(item.getID()));
+            FindAndTextView(R.id.unit_price_view,String.valueOf(item.getUnitPrice()));
+            FindAndTextView(R.id.count_view,String.valueOf(item.getCount()));
+            FindAndTextView(R.id.total_view,String.valueOf(item.getCount()*item.getUnitPrice()));
+            parent.addView(TR);
+        }
+
+        public ViewHolder(TableLayout parent,String s,long total,int count) {
+            inflater = getLayoutInflater();
+            TR = (TableRow) inflater.inflate(R.layout.table_row,parent,false);
+            for (int i = 0; i < TR.getChildCount(); i++) {
+                ((TextView)TR.getChildAt(i)).setTextAppearance(R.style.table_layout_style_last_row_appearance);
+            }
+            FindAndTextView(R.id.item_name_view,s);
+            FindAndTextView(R.id.total_view,String.valueOf(total));
+            FindAndTextView(R.id.count_view,String.valueOf(count));
+            FindAndTextView(R.id.number_view,"*");
+            parent.addView(TR);
+        }
+
+
+        public void FindAndTextView(@IdRes int id, String text){
+            ((TextView) TR.findViewById(id)).setText(text);
+        }
     }
 }
--- app/src/main/java/ir/saeed/start/livadata/MainActivity.java
+++ app/src/main/java/ir/saeed/start/livadata/MainActivity.java
@@ -1,10 +1,9 @@
 package ir.saeed.start.livadata;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -43,7 +42,7 @@ public class MainActivity extends AppCompatActivity{
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.get_result:
-                onClick();
+                goResult();
                 break;
             case R.id.close:
                 (new AlertDialog.Builder(this)).setMessage("Are you sure to exit? ").setCancelable(false).setPositiveButton("Exit now!",(dialog, which) -> finish()).setNegativeButton("Cancel",((dialog, which) -> dialog.cancel())).create().show();
@@ -54,9 +53,8 @@ public class MainActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick() {
-        int t = 0;
-        for (CellItem item:db.getItemsList()) t += item.getCount()*item.getUnitPrice();
-        (new AlertDialog.Builder(this)).setTitle("Factor Sum: ").setMessage("Total: "+t+"\nCount: " +db.getCount()).create().show();
+    public void goResult() {
+        Intent i = new Intent(this,FactorResult.class);
+        startActivity(i);
     }
 }
